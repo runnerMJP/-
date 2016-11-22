ec0cdeb7bc41a3a58cd466ca0cf6d4bd23d20e5e
runnerMJP <1249895792@qq.com>
2016-11-22 10:52:47 +0800

    初始化项目

---
+++ meituan/meituan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  meituan
+//
+//  Created by MJP on 16/11/22.
+//  Copyright © 2016年 YSAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ meituan/meituan/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  meituan
+//
+//  Created by MJP on 16/11/22.
+//  Copyright © 2016年 YSAN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
