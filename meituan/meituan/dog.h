2adcae780930658c7143f25c0782914750844a2c
runnerMJP <1249895792@qq.com>
2016-11-22 10:59:53 +0800

    创建了dog类

---
+++ meituan/meituan/dog.h
@@ -0,0 +1,15 @@
+//
+//  dog.h
+//  meituan
+//
+//  Created by MJP on 16/11/22.
+//  Copyright © 2016年 YSAN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dog : NSObject
+
+@property (copy,nonatomic) NSString * name;
+
+@end
---
+++ meituan/meituan/dog.m
@@ -0,0 +1,13 @@
+//
+//  dog.m
+//  meituan
+//
+//  Created by MJP on 16/11/22.
+//  Copyright © 2016年 YSAN. All rights reserved.
+//
+
+#import "dog.h"
+
+@implementation dog
+
+@end
